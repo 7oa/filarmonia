aa0ec12c2a7eb4f0a8aa893bf6ed0deb2a85d19a
7oa <semenova.7oa@yandex.ru>
2017-09-12 17:58:59 +0300

    new pages

---
+++ app/js/scripts/script.js
@@ -0,0 +1,9 @@
+// js jquery
+$(document).ready(function() {
+	var swiper = new Swiper('.slider', {
+		pagination: '.slider__pagination',
+		paginationClickable: true,
+		preventClicks: false,
+		preventClicksPropagation: false
+	});
+});
