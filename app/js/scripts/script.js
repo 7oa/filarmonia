3fcb456ce31e22eec12f6045e6abc3290789c108
7oa <semenova.7oa@yandex.ru>
2017-09-13 13:26:02 +0300

    new pages

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -6,4 +6,14 @@ $(document).ready(function() {
 		preventClicks: false,
 		preventClicksPropagation: false
 	});
+	if($(window).width()>=1240){
+		var swiper = new Swiper('.news-slider', {
+			slidesPerView: 3,
+			spaceBetween: 60,
+			nextButton: '.news-slider__next',
+			prevButton: '.news-slider__prev',
+			preventClicks: false,
+			preventClicksPropagation: false
+		});
+	}
 });
